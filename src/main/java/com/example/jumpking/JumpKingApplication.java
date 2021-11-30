df34a692e446ae9f258170a0a902fd58812f8685
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-11-30 12:32:09 +0900

    first commit

---
+++ src/main/java/com/example/jumpking/JumpKingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jumpking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpKingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumpKingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jumpking/JumpKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jumpking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpKingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
